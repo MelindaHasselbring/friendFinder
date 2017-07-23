d33907c4f849627d1148bdcd71226f8e8aff72a7
Melinda Hasselbring <melinda.hasselbring@gmail.com>
2017-07-22 20:27:34 -0700

    added more files

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,16 @@
+// Dependencies
+// =============================================================
+var path = require('path');
+
+
+module.exports = function(app) {
+  // define the home page route
+  app.get('/', function(req, res) {
+      res.sendFile(path.join(__dirname, '../public/home.html'));
+  });
+
+  // define the about route
+  app.get('/survey', function(req, res) {
+      res.sendFile(path.join(__dirname, '../public/survey.html'));
+  });
+};
